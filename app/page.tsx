d17bcba881018edb8689bbd30a197618af4d7a7c
Shane Richards <shane.richards121@gmail.com>
2026-09-25 00:16:59 -0400

    feat: adding the planner to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -45,9 +45,9 @@ export default function HomePage() {
         </div>
       </header>
 
-      <HomeWeekStrip />
-
       <HomeStartWorkout />
+
+      <HomeWeekStrip />
 
       <HomeWorkoutActivity />
 
